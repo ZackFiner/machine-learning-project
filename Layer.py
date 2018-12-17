fixed overflow issues

Layer.py
@@ -11,6 +11,7 @@ def sigmoid(nValue):
 
 def sigmoidPrime(nValue):
     d = sigmoid(nValue)
+    #return np.exp(-nValue)*d*d
     return np.exp(-nValue)*d*d
 
 class layer(object):
@@ -47,14 +48,14 @@ class layer(object):
             return #terminate, we don't need to set the weights or bias as the activation of this layer will be input
         
          
-        self.m_weights = np.random.rand(self.m_size,lastLayer.m_size) # randomize the weights to the last layer (*)
-        self.m_bias = np.random.rand(self.m_size) # allocate a column vector of biases, initially all of 1 (i chose this arbitrarily)
+        self.m_weights = 2*np.random.rand(self.m_size,lastLayer.m_size)-1 # randomize the weights to the last layer (*)
+        self.m_bias = 2*np.random.rand(self.m_size)-1 # allocate a column vector of biases, initially all of 1 (i chose this arbitrarily)
         self.m_lastLayer = lastLayer
     
     def weightedInput(self):
         v = np.dot(self.m_weights, self.m_lastLayer.m_activation)
         return v + self.m_bias#calculate weighted input
-    
+
     def feedforward(self):    
         rawM = self.weightedInput() #calculate weighted input
         mapFunc = np.vectorize(sigmoid) # use the vectorize interface to get a mapping function with sigmoid
@@ -68,7 +69,7 @@ class layer(object):
     def setActivation(self, data):
         if self.m_isInput:
             mapFunc = np.vectorize(sigmoid)
-            self.m_activation = np.asarray(mapFunc(data))
+            self.m_activation = data
     
     def getMaxIndex(self):
         return np.argmax(self.m_activation)

Network.py
@@ -26,8 +26,9 @@ class Network(object):
     m_outputLayer = None
     m_trainingExamples = None
     m_trainingSolutions = None
-    m_currentError = [None]*4
-    m_numberOfLayers = 4
+    m_numberOfLayers = 3
+    m_currentError = [None]*m_numberOfLayers
+
     TRAININGTIME = 50000
     def __init__(self):
         '''
@@ -40,9 +41,9 @@ class Network(object):
         self.m_inputLayer = layer(INPUT_IMAGE_SIZE, None)
         self.m_layers.insert(0, self.m_inputLayer )
         self.m_layers.insert(1, layer(HIDDEN_NEURON_NUM, self.m_layers[0]))
-        self.m_layers.insert(2, layer(HIDDEN_NEURON_NUM, self.m_layers[1]))
-        self.m_outputLayer = layer(10, self.m_layers[2]) # output layer for image
-        self.m_layers.insert(3, self.m_outputLayer)
+        #self.m_layers.insert(2, layer(HIDDEN_NEURON_NUM, self.m_layers[1]))
+        self.m_outputLayer = layer(10, self.m_layers[1]) # output layer for image
+        self.m_layers.insert(2, self.m_outputLayer)
 
 
     def getCost(self, expectedValue):
@@ -57,6 +58,7 @@ class Network(object):
         returns the error for a given input
         '''
         diff = self.m_outputLayer.m_activation - expectedValue
+        v = self.m_outputLayer.getPrimeWeighted()
         return np.multiply(diff, self.m_outputLayer.getPrimeWeighted())
 
     def calcError(self, expectedValue):
@@ -65,12 +67,16 @@ class Network(object):
         self.m_currentError.insert(numOfLayers-1, localLastError) # insert the first error ~(expected - what we got for output)
         # iteratively determine the error of a specific layer, moving backward from the given error
         for index in range(numOfLayers-2, 0, -1): #loop backwards from the second to last layer to the second layer (we don't update the input layer!)
-            leftSide = np.dot(self.m_layers[index+1].m_weights.transpose(), localLastError) # index + 1 will be the layer in front of this one
+            leftSide = np.dot(self.m_layers[index+1].m_weights.T, localLastError) # index + 1 will be the layer in front of this one
             rightSide = self.m_layers[index].getPrimeWeighted() # we get the activations for the current layer, and return the prime weighted value
             localLastError = np.multiply(leftSide, rightSide) # hadamard product between the prime weighted and
             self.m_currentError[index] = localLastError
 
         return localLastError
+    def runInput(self, array):
+        self.m_inputLayer.setActivation(array)
+        for x in range(1, self.m_numberOfLayers):
+            self.m_layers[x].feedforward()
 
     def trainNetwork(self):
         '''
@@ -82,51 +88,87 @@ class Network(object):
         stoischratic gradient descent (because it updates the network after each test)
         instead of batch gradient descent (where we would compile the data from each test, and then process it)
         '''
-        for epochIndex in range(0, 3): # for each example in the number of training epochs
+        for epochIndex in range(0, 1): # for each example in the number of training epochs
             i = 0
             p=0
             print('epoch'+str(epochIndex))
             for trainingImage in self.m_trainingExamples: # for each training example
                 if 0==(i % 1000):
+                    if (p!=0):
+                        print("solution for 7")
+                        print(self.getSolution(self.m_trainingExamples[0]))
                     print(str(p*10)+'%')
                     p+=1
-                self.m_inputLayer.setActivation(np.array(trainingImage))
-
-                for x in range(1, self.m_numberOfLayers):
-                    self.m_layers[x].feedforward()
+                imgdata = np.array(trainingImage)
+                self.runInput(imgdata)
                 #gradientConstant = self.TRAININGTIME / len(self.m_trainingExamples)
                 gradientConstant = 1.0
                 self.calcError(toNumpyArray(self.m_trainingSolutions[i], 10)) # calculate the error for each layer
                 i += 1
                 for x in range(1,self.m_numberOfLayers):
                     layerError = self.m_currentError[x]
-                    #print(layerError)
-                    self.m_layers[x].m_bias = self.m_layers[x].m_bias - np.multiply(gradientConstant, layerError)
-                    #print(self.m_layers[x - 1].m_activation.shape)
-                    #print(self.m_layers[x-1].m_activation[np.newaxis].shape)
-                    rightSide = np.dot(layerError[np.newaxis].T, (self.m_layers[x-1].m_activation)[np.newaxis]) # unfortunatley, we need to do some weird axis splitting here to get the behavior we want (column vector * row vector)
-                    leftSide = np.multiply(gradientConstant,self.m_layers[x].m_weights)
+                    self.m_layers[x].m_bias = self.m_layers[x].m_bias - gradientConstant*layerError
+                    rightSide = np.dot(layerError[np.newaxis], self.m_layers[x-1].m_activation[np.newaxis].T) # unfortunatley, we need to do some weird axis splitting here to get the behavior we want (column vector * row vector)
+                    leftSide = gradientConstant*self.m_layers[x].m_weights
                     finalsub = np.multiply(leftSide, rightSide)
-                    #print(finalsub.shape)
-                    self.m_layers[x].m_weights = np.subtract(self.m_layers[x].m_weights, finalsub)
-            print('solution for 2:')
-            print(self.getSolution(self.m_trainingExamples[0]))
+                    self.m_layers[x].m_weights = self.m_layers[x].m_weights - finalsub
+
+            print("solution for 7")
+            print(self.getMax(self.getSolution(self.m_trainingExamples[0]))[7]==1)
+    def getMax(self, array):
+
+        vMax = -1
+        for x in array:
+            if (x>vMax):
+                vMax = x
+
+        def filter(x):
+            if (x/vMax != 1):
+                return 0
+            return 1
+        return np.asarray(np.vectorize(filter)(array))
     def getSolution(self, image):
         self.m_inputLayer.setActivation(image)
         for x in range(1, self.m_numberOfLayers):
             self.m_layers[x].feedforward()
-            print(self.m_layers[x].m_activation)
         return self.m_outputLayer.m_activation
+    def testReults(self):
+        successes = 0
+
+        for x in range(0, self.m_trainingExamples.shape[0]):
+            img = self.m_trainingExamples[x]
+            self.runInput(img)
+            correct = self.m_trainingSolutions[x]
+            if (self.getMax(self.getSolution(self.m_trainingExamples[x]))[correct]==1):
+                successes+=1
+
+        print("Neural Network can correctly identify: ")
+        print(successes/100)
+        print("percent of training examples")
+
+
 def toNumpyArray(value, max):
     returnVal = np.zeros(max)
     returnVal.flat[value] = 1
     return returnVal
 
+def printImg(array):
+    for x in range(0, 28):
+        string = ""
+        for y in range(0,28):
+            if (array[x*28+y] > 50):
+                string +="_"
+            else:
+                string+= " "
+        print(string)
 def makeColumnVec(arraylike):
     return np.asmatrix(arraylike).reshape(len(arraylike),1)
 mndata = MNIST('samples')
 images, labels = mndata.load_testing()
 d = Network()
-d.m_trainingExamples = images
+d.m_trainingExamples = np.asarray(np.vectorize(lambda x: x/255)(images))
+printImg(images[1])
+print(labels[1])
 d.m_trainingSolutions = labels
 d.trainNetwork()
+d.testReults()